9a2d60d07ae00838848f9dd62a18267a072410f3
reVInotip <grigoriy.novikov220@gmail.com>
2024-07-16 11:31:56 +0700

    First lab created

---
+++ src/backend/main.c
@@ -0,0 +1,41 @@
+#include <dlfcn.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <libconfig.h>
+#include <string.h>
+#include <assert.h>
+#include "../include/shlib_operations/lib_stack.h"
+#include "../include/shlib_operations/loader.h"
+
+/*
+descr: Initialize all extensions
+*/
+void init_all_exetensions(Lib_stack_ptr lib_stack)
+{
+    void (*function)() = dlsym(top(lib_stack), "init");
+    function();
+}
+
+/*
+descr: Move this to config
+notes: Later this absolute path can be replaced on relative by LD_LIBRARY_PATH variable (when will they study them)
+*/
+char *base_dir = "/home/grisha/Projects/oos-proxy/src/backend/contrib"; // enter your path
+
+int main(int argc, char *argv[]) {
+    //read_config
+    //init_logger
+
+    Lib_stack_ptr lib_stack = create_stack();
+    loader(&lib_stack, base_dir);
+
+    if (get_size(lib_stack) > 0)
+    {
+        init_all_exetensions(lib_stack);
+    }
+    else
+    {
+        // add log record
+    }
+
+}
---
+++ src/include/shlib_operations/loader.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "lib_stack.h"
+
+/*
+descr: Find and load all shared libraties
+args: stack - stack of shared libraries, path_to_source - path to the directory containing extensions,
+*/
+#define loader(stack, path_to_source) \
+    do \
+    {\
+        default_loader(stack, path_to_source, 0, 1); \
+    } while (0)
+
+extern void default_loader(Lib_stack_ptr *stack, char *path_to_source, int curr_depth, const int depth);
